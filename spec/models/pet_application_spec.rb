00ea4d217b79433aa1cd35cb888774b552d75585
Tyler Noble <tmnoble11@gmail.com>
2024-07-11 19:29:55 -0600

    added: relationship test to pet_application_spec.rb

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,3 +1,8 @@
 require "rails_helper"
 
-RSpec.describe
+RSpec.describe PetApplication, type: :model do 
+  describe "relationships" do 
+    it { should belong_to :pet }
+    it { should belong_to :application }
+  end
+end
