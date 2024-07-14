0f74688a7d7753d8d98bb4ac214982bb3ba493ed
Tyler Noble <tmnoble11@gmail.com>
2024-07-13 18:53:04 -0600

    added url to app page on pet page, added get/post routes for application, added applications/new view and updated controller. Wrote tests.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,4 +3,27 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @pets = @application.pets
   end
+
+  def new
+  end
+
+  def create
+    @application = Application.new(application_params)
+
+    if @application.save
+      redirect_to "/applications/#{Application.last.id}"
+    end
+  end
+
+  private
+  def application_params
+    params.permit(
+      :applicant_name,
+      :street_address,
+      :city,
+      :state,
+      :zip,
+      :description
+    ).merge(status: "In Progress")
+  end
 end
